3cbb987c8be191bba101813f5e38ccf7ab420cda
Warren Alphonso <warrenalphonso02@gmail.com>
2018-12-19 11:13:35 -0800

    added paddles and ball

---
+++ app.py
@@ -0,0 +1,39 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Warren, created using a freeCodeCamp tutorial")
+wn.bgcolor("black")
+wn.setup(width = 800, height = 600)
+wn.tracer(0)
+
+#paddle a
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+#paddle b
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+#ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.shapesize(stretch_wid=1, stretch_len=1)
+ball.penup()
+ball.goto(0, 0)
+
+
+#main game loop
+while True:
+    wn.update()
